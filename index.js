1f53ec4e32a93b0f5bf1edb8ab0b63c9bb858c37
--add <andrewksmoker@gmail.com>
2021-01-29 15:55:52 -0500

    commit tree 3cb90874630edb74316e2e6a702968eab2337159

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    let results = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+    return results 
+}
+
+function fuzzyMatch(drivers, string) {
+    let results = drivers.filter(driver => driver.toLowerCase().indexOf(string.toLowerCase()) === 0)
+    return results 
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+}
